b4e30c7a8fbce6a9afae8c1852c0efd08fbae6c4
Pollito-0601 <159592838+Pollito-0601@users.noreply.github.com>
2026-08-20 22:32:24 -0500

    El static fue removido.

--- Sistema de conteo de horas/src/horasextra/logica/ConversorTiempo.java
+++ Sistema de conteo de horas/src/horasextra/logica/ConversorTiempo.java
@@ -1,14 +1,14 @@
 package horasextra.logica;
 
 public class ConversorTiempo {
-    public static int convertirAMinutos(boolean signo, int horas, int minutos){
+    public int convertirAMinutos(boolean signo, int horas, int minutos){
         int totalMinutos = minutos + (horas*60);
         if (!signo){
             totalMinutos = -totalMinutos;
         }
         return totalMinutos;
     }
-    public static int[] convertirAHoras(int minutos){
+    public int[] convertirAHoras(int minutos){
         int[] totalHoras = new int[3];
         int horas = Math.abs(minutos / 60);
         int mins = Math.abs(minutos % 60);
